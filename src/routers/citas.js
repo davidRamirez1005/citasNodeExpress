03938e36444769482537f297cfbdce5cda0df2bf
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-15 22:47:04 -0500

    feat: :sparkles: get a citas_medicas
    para obtener las citas de los usuarios

---
+++ src/routers/citas.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import con from '../../server/db.js'
+import { plainToClass } from 'class-transformer';
+
+
+const appCitas = express.Router();
+appCitas.use(express.json());
+/**
+ *  ! Obtener todas las citas alfabéticamente
+ */
+appCitas.get('/:id?', (req, res) => {
+(req.params.id)
+    con.query(
+    /* sql */'SELECT cita.*, usuario.usu_nombre, usuario.usu_primer_apellido FROM cita INNER JOIN usuario ON cita.cit_datosUsuario = usuario.usu_id ORDER BY usuario.usu_nombre, usuario.usu_primer_apellido',
+    (err, data, fils) => {
+    res.send(data);
+    },
+    );
+});
+export default appCitas
