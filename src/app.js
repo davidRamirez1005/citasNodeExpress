e711f7744947da9e7cd0fd4dea7a31bf88316d5d
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-15 22:47:44 -0500

    feat: :sparkles: enpoints
    pacientes y citas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { servidor } from '../server/db.js';
 import appPacientes from './routers/pacientes.js';
+import appCitas from './routers/citas.js';
+import appEspecialidad from './routers/especialidad.js';
 
 dotenv.config();
 const appExpress = express();
@@ -10,6 +12,10 @@ const appExpress = express();
 appExpress.use(express.json());
 
 appExpress.use('/paciente', appPacientes);
+appExpress.use('/cita', appCitas);
+appExpress.use('/especialidad', appEspecialidad);
+
+
 
 appExpress.listen(servidor.port, () => {
     console.log(`Servidor escuchando en http://${servidor.addresses}:${servidor.port}/`);
